9b8ac5f559f17b20ed1a55ccd9a7f3cf65c29db4
Jason Huang <cjhuang@oath.com>
2018-02-22 00:40:54 +0800

    support TSE/OTC

--- packages/twse/src/getStockInfo.js
+++ packages/twse/src/getStockInfo.js
@@ -1,18 +1,39 @@
 import axios from 'axios';
 import getCookie from './getCookie';
+import getOTCStocks from './getOTCStocks';
+import getTSEStocks from './getTSEStocks';
 
 const TWSE_STOCK_API = `http://mis.twse.com.tw/stock/api/getStockInfo.jsp`;
+
+const getFullSymbol = symbol => {
+    const otcStocks = getOTCStocks();
+    const tseStocks = getTSEStocks();
+
+    if (otcStocks[symbol]) {
+        return `otc_${symbol}.tw`;
+    }
+
+    if (tseStocks[symbol]) {
+        return `tse_${symbol}.tw`;
+    }
+
+    return '';
+};
 
 export default async (symbols = []) => {
     try {
         const response = await axios.get(TWSE_STOCK_API, {
             headers: { Cookie: await getCookie() },
             params: {
-                ex_ch: symbols.map(symbol => `tse_${symbol}.tw`).join('|'),
+                ex_ch: symbols
+                    .map(getFullSymbol)
+                    .filter(symbol => !!symbol)
+                    .join('|'),
                 _: new Date().getTime()
             }
         });
         const { data: { msgArray: stockInfo = [] } = {} } = response;
+        console.log(stockInfo);
 
         return stockInfo;
     } catch (e) {
