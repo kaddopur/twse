ef73a38e5e68f89552cd30a5640115ebe224edcb
Jason Huang <cjhuang@oath.com>
2018-02-23 23:09:57 +0800

    fix typo

--- packages/twse-cli/src/store.js
+++ packages/twse-cli/src/store.js
@@ -1,11 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import screenReducer from './ducks/screen';
-import symbolRecuder from './ducks/symbol';
+import symbolReducer from './ducks/symbol';
 import conf from './conf';
 
 const rootReducer = combineReducers({
     screen: screenReducer,
-    symbol: symbolRecuder
+    symbol: symbolReducer
 });
 
 const initialState = {
